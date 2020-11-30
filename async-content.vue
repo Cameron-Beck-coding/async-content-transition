da599e39967eaf247e032f745ae8d04e3f4acc5b
Cameron-Beck <cjsoccer62@gmail.com>
2020-11-30 14:29:52 -0600

    initial commit

---
+++ async-content.vue
@@ -0,0 +1,66 @@
+<template>
+    <transition name="fade">
+        <div>
+            <div v-if="!loaded && showSpinner" name="loading-content">
+                <div class="loading-content-div">
+                    <div class="spinner"></div>
+                </div>
+            </div>
+            <slot v-if="loaded">
+            </slot>
+        </div>
+    </transition>
+</template>
+<style>
+loading-content-div {
+    width: 100%;
+    height: 100%;
+}
+
+.spinner {
+    width: 40px;
+    height: 40px;
+    margin: 100px auto;
+    background-color: #333;
+    border-radius: 100%;
+    -webkit-animation: sk-scaleout 1.0s infinite ease-in-out;
+    animation: sk-scaleout 1.0s infinite ease-in-out;
+}
+
+@-webkit-keyframes sk-scaleout {
+    0% {
+        -webkit-transform: scale(0)
+    }
+    100% {
+        -webkit-transform: scale(1.0);
+        opacity: 0;
+    }
+}
+
+@keyframes sk-scaleout {
+    0% {
+        -webkit-transform: scale(0);
+        transform: scale(0);
+    }
+    100% {
+        -webkit-transform: scale(1.0);
+        transform: scale(1.0);
+        opacity: 0;
+    }
+}
+</style>
+<script>
+import 'whatwg-fetch'
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+export default {
+    name: 'async-content',
+    props: {
+        loaded: undefined,
+        showSpinner: {
+            default: true
+        }
+    }
+}
+</script>
