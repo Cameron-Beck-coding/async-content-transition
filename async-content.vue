8016fdd23ae0466f3a40548fb7443d7499024d2b
Cameron-Beck <cjsoccer62@gmail.com>
2020-12-03 10:19:15 -0600

    fixed incorrect mode

--- async-content.vue
+++ async-content.vue
@@ -1,61 +1,59 @@
 <template>
-    <transition name="fade">
-        <div>
-            <div v-if="!loaded && showSpinner" name="loading-content">
-                <div class="loading-content-div">
-                    <div class="spinner"></div>
-                </div>
-            </div>
-            <slot v-if="loaded">
-            </slot>
-        </div>
-    </transition>
+  <transition name="fade" mode="out-in">
+    <div v-if="!loaded && showSpinner" key="async-content-spinner" name="loading-content">
+      <div class="loading-content-div">
+        <div class="spinner"></div>
+      </div>
+    </div>
+    <div v-else-if="loaded" key="async-content-slot-container">
+      <slot> </slot>
+    </div>
+  </transition>
 </template>
 <style>
 loading-content-div {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 .spinner {
-    width: 40px;
-    height: 40px;
-    margin: 100px auto;
-    background-color: #333;
-    border-radius: 100%;
-    -webkit-animation: sk-scaleout 1.0s infinite ease-in-out;
-    animation: sk-scaleout 1.0s infinite ease-in-out;
+  width: 40px;
+  height: 40px;
+  margin: 100px auto;
+  background-color: #333;
+  border-radius: 100%;
+  -webkit-animation: sk-scaleout 1s infinite ease-in-out;
+  animation: sk-scaleout 1s infinite ease-in-out;
 }
 
 @-webkit-keyframes sk-scaleout {
-    0% {
-        -webkit-transform: scale(0)
-    }
-    100% {
-        -webkit-transform: scale(1.0);
-        opacity: 0;
-    }
+  0% {
+    -webkit-transform: scale(0);
+  }
+  100% {
+    -webkit-transform: scale(1);
+    opacity: 0;
+  }
 }
 
 @keyframes sk-scaleout {
-    0% {
-        -webkit-transform: scale(0);
-        transform: scale(0);
-    }
-    100% {
-        -webkit-transform: scale(1.0);
-        transform: scale(1.0);
-        opacity: 0;
-    }
+  0% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  100% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    opacity: 0;
+  }
 }
 </style>
 <script>
-import 'whatwg-fetch'
 export default {
-    props: {
-        loaded: undefined,
-        showSpinner: {
-            default: true
-        }
-    }
-}
+  props: {
+    loaded: undefined,
+    showSpinner: {
+      default: true,
+    },
+  },
+};
 </script>
